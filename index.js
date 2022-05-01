18a95f8f03d8db82d28afff6cbbdda11a5421ccd
TsvyatkoIvanov <tsvqtkoivanov@gmail.com>
2022-05-01 15:08:18 +0300

    init implementation

---
+++ index.js
@@ -0,0 +1,91 @@
+let urlsList = [];
+let currentImgUrl;
+
+const buttonYes = document.getElementById("button-yes");
+const buttonNo = document.getElementById("button-no");
+
+const urlPreview = document.getElementById("url-preview");
+const fileSelect = document.getElementById("file-select");
+const noMoreUrlsDiv = document.getElementById("no-more-urls-header");
+
+fileSelect.addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  let reader = new FileReader();
+
+  reader.onload = function (event) {
+    let str = event.target.result;
+    try {
+      urlsList = JSON.parse(str);
+      currentImgUrl = urlsList[0];
+      setNewImage(currentImgUrl);
+      window.localStorage.setItem("favorite-urls", JSON.stringify([]));
+      window.localStorage.setItem("imported-urls", JSON.stringify(urlsList));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Read the file
+  reader.readAsText(file);
+});
+
+function removeUrlFromOriginalList(url) {
+  try {
+    const urlsString = window.localStorage.getItem("imported-urls");
+    let urlsList = urlsString ? JSON.parse(urlsString) : [];
+    const index = urlsList.findIndex(function (u) {
+      return u === url;
+    });
+    if (index > -1) {
+      urlsList.splice(index, 1);
+      if (urlsList.length > 0) {
+        currentImgUrl = urlsList[0];
+        setNewImage(currentImgUrl);
+      } else {
+        noMoreUrlsDiv.classList = "flex";
+      }
+      window.localStorage.setItem("imported-urls", JSON.stringify(urlsList));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function addUrlToFav(url) {
+  try {
+    const favUrlsString = window.localStorage.getItem("favorite-urls");
+    let favUrls = [];
+    if (favUrlsString) {
+      favUrls = JSON.parse(favUrlsString);
+      favUrls.push(url);
+    } else {
+      favUrls.push(url);
+    }
+    window.localStorage.setItem("favorite-urls", JSON.stringify(favUrls));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function setNewImage(url) {
+  urlPreview.src = url;
+}
+
+function onClickYes() {
+  addUrlToFav(currentImgUrl);
+  removeUrlFromOriginalList(currentImgUrl);
+}
+
+function onClickNo() {
+  removeUrlFromOriginalList(currentImgUrl);
+}
+
+function getList() {
+  try {
+    const favUrlsString = window.localStorage.getItem("favorite-urls");
+    favUrls = JSON.parse(favUrlsString);
+    console.log("favUrls: ", favUrls);
+  } catch (e) {
+    console.log(e);
+  }
+}
